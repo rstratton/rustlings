cdd6df6522322dfc3ff430654243f8b0a258548f
Robert Stratton <bobby@middesk.com>
2020-05-02 11:09:22 -0700

    Add my solutions so far

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,9 @@
 // Create an array with at least 100 elements in it where the ??? is. 
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // So [u32; 100] is a valid type, while [0; 100] is a valid array initializer
+    let a = [0; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
